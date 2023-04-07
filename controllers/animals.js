bb5e0f616e3ccbfc16191d8f863703f7fe991bbe
Wesner M <wesnerm@gmail.com>
2023-04-07 14:47:55 -0400

    Last minute changes

--- controllers/animals.js
+++ controllers/animals.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const startAnimals = require("../db/animalSeed.js");
 const Animal = require("../models/animals");
 
-// router.use(express.urlencoded({ extended: false }));
-
 // INDEX
 router.get("/", async (req, res) => {
   const animals = await Animal.find({});
@@ -26,16 +24,14 @@ router.post("/", async (req, res) => {
 // SHOW
 router.get("/show/:id", async (req, res) => {
   const animal = await Animal.findById(req.params.id);
-  console.log("animal", animal);
   res.render("show.ejs", { animal: animal });
 });
 
 // EDIT
 router.get("/edit/:id/", async (req, res) => {
   const animal = await Animal.findById(req.params.id);
-  // const index = animals.indexOf(animal);
+
   res.render("edit.ejs", { animal });
-  //line above I removed , { animal: animal, index: index });
 });
 
 // SEED
@@ -53,31 +49,14 @@ router.put("/:id", async (req, res) => {
   const animal = await Animal.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-  res.redirect("/fruits");
-});
-
-// router.put("/:id", async (req, res) => {
-//   const animal = await Animal.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//   });
-//   res.redirect("/animals");
-// });
-
-// // DELETE
-
-router.delete("/delete/:id", async (req, res) => {
-  const animal = await Animal.findByIdAndDelete(req.params.id);
   res.redirect("/animals");
 });
 
-// router.get("/delete/:id", async (req, res) => {
-//   const animalToDelete = await Animal.findById(req.params.id);
-//   res.render("/animals/delete.ejs", { animalToDelete });
-// });
-// router.delete("/delete/:id", async (req, res) => {
-//   const animal = await Animal.findByIdAndDelete(req.params.id);
-//   res.send({ success: true, animal: animal });
-//   // res.redirect("/animals")
-// });
+// DELETE
+
+router.delete("/:id", async (req, res) => {
+  const animal = await Animal.findByIdAndDelete(req.params.id);
+  res.redirect("/animals");
+});
 
 module.exports = router;
